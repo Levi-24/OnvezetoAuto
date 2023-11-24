453892ed28e07532a6c2e2fb80b297a41e70849b
Levi-24 <leventemajor36@gmail.com>
2023-11-24 09:52:30 +0100

    Add project files.

---
+++ Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onvezeto_Major_Levente
+{
+    class Auto
+    {
+        public string gyartoModell { get; set; }
+        public string SAESzint { get; set; }
+        public int aktulaisSebesseg { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Onvezeto_Major_Levente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
